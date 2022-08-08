b20fbc725679cb15477c5177f65956f90331bf37
Leandro Silva Luz do Carmo <leandrosluzcarmo@gmail.com>
2022-08-07 22:44:45 -0300

    improvement: Removed tailwind and classnames dependencies

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,5 @@
 import { AppProps } from 'next/app'
 import { GlobalStyles } from '@/src/theme'
-import '../styles/index.css'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -44,6 +44,10 @@ export default class MyDocument extends Document {
             rel="stylesheet"
             href="https://fonts.googleapis.com/css2?family=Work+Sans:ital,wght@0,400;0,600;1,400;1,600&display=swap"
           />
+          <link
+            rel="stylesheet"
+            href="https://unpkg.com/tailwindcss@3.1.8/src/css/preflight.css"
+          />
         </Head>
         <body>
           <GlobalStyles />
