9b9e43d796d676f98721fb34a2fd6866773498ea
Leandro Silva Luz do Carmo <leandrosluzcarmo@gmail.com>
2023-05-18 23:16:34 -0300

    improvement: Set the html lang attribute according to current locale

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -37,7 +37,7 @@ export default class MyDocument extends Document {
 
   render() {
     return (
-      <Html lang="en">
+      <Html>
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link
