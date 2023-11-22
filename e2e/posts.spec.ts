fcd066decd21ed32fbbf0b631a3369270db4b727
Leandro Silva Luz do Carmo <leandrosluzcarmo@gmail.com>
2023-11-22 12:31:50 -0300

    tests: Add posts page tests

--- e2e/posts.spec.ts
+++ e2e/posts.spec.ts
@@ -24,11 +24,11 @@ const assertPostPreviewIsVisible = async (
 }
 
 test.beforeEach(async ({ page }) => {
-  await page.goto('/')
+  await page.goto('/posts')
 })
 
 test.describe('Home', () => {
-  test('Should show post previews', async ({ page }) => {
+  test('Should show all post previews', async ({ page }) => {
     await assertPostPreviewIsVisible(
       page,
       'Hello world: Announcing my new site',
@@ -46,6 +46,24 @@ test.describe('Home', () => {
     )
   })
 
+  test('Should only show matched search posts', async ({ page }) => {
+    const searchInput = page.locator('input[type=search]')
+
+    await searchInput.fill('ano')
+
+    await assertPostPreviewIsVisible(
+      page,
+      'A tool vs another new tool',
+      '10 may 2022',
+      '/assets/blog/tool-vs-tool/cover.jpg',
+      ['Tools'],
+    )
+
+    await expect(
+      page.locator('h1', { hasText: 'Hello world: Announcing my new site' }),
+    ).toBeHidden()
+  })
+
   test('Should navigate to post when clicking post preview', async ({
     page,
   }) => {
@@ -60,21 +78,5 @@ test.describe('Home', () => {
     })
 
     await expect(page.locator('h1', { hasText: 'Hello World' })).toBeVisible()
-  })
-
-  test('Should navigate to posts page when clicking see all posts', async ({
-    page,
-  }) => {
-    const link = page.locator('a', {
-      hasText: /See all blog posts/i,
-    })
-
-    await link.click()
-
-    await page.waitForURL('/posts', {
-      timeout: 10 * 1000,
-    })
-
-    await expect(page.locator('h1', { hasText: 'Blog' })).toBeVisible()
   })
 })
