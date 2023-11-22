965157a5d0a5eeb86769241f883647c841ad191a
Leandro Silva Luz do Carmo <leandrosluzcarmo@gmail.com>
2023-11-22 00:07:03 -0300

    feat: Posts page translation

--- src/components/posts/NoResults/NoResults.tsx
+++ src/components/posts/NoResults/NoResults.tsx
@@ -1,5 +1,6 @@
 import { ForwardedRef, forwardRef } from 'react'
 import { FiFolder, FiSearch } from 'react-icons/fi'
+import { useTranslation } from '@/src/hooks/useTranslation'
 import * as S from './NoResults.styles'
 
 type Props = {
@@ -10,13 +11,15 @@ const InnerNoResults = (
   { className }: Props,
   ref: ForwardedRef<HTMLElement>,
 ) => {
+  const { translate } = useTranslation()
+
   return (
     <S.Container className={className} ref={ref as React.RefObject<never>}>
       <S.IconContainer aria-hidden="true">
         <S.Icon as={FiFolder} />
         <S.AboveIcon as={FiSearch} />
       </S.IconContainer>
-      <S.Text>Nenhuma postagem encontrada</S.Text>
+      <S.Text>{translate('posts.nothing-found')}</S.Text>
     </S.Container>
   )
 }
