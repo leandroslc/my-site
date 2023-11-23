72a9d88f40cfee1b357f3ed54cd4fa3fdf76f2a9
Leandro Silva Luz do Carmo <leandrosluzcarmo@gmail.com>
2023-11-23 12:16:13 -0300

    fix: Copy posts data files to output (#13)

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,17 +1,17 @@
-import { join } from 'path'
+import { join, resolve } from 'path'
 
 export const postsDirectory = join(process.cwd(), process.env.POSTS_DIRECTORY!)
 
-export const postsDataFilePath = join(
-  process.cwd(),
-  process.env.POSTS_DATA_DIRECTORY!,
-)
-
 export const translatedPagesDirectory = join(
   process.cwd(),
-  'src',
-  'locales',
-  'pages',
+  process.env.TRANSLATED_PAGE_LOCALES_DIRECTORY ?? '',
+)
+
+// 'resolve' with a path as string constant instructs the build to
+// copy the given path to output
+export const postsDataFilePath = resolve(
+  './_posts/data',
+  process.env.POSTS_DATA_RELATIVE_DIRECTORY ?? '',
 )
 
 export const baseUrl = process.env.BASE_URL!
