ddfaf5e4f0830ed082c57ee652c27dfa6018de08
Leandro Silva Luz do Carmo <leandrosluzcarmo@gmail.com>
2023-11-23 21:40:01 -0300

    feat: Sitemap

---
+++ src/app/sitemap.ts
@@ -0,0 +1,58 @@
+import { MetadataRoute } from 'next'
+import { DEFAULT_LOCALE, Locales } from '@/src/config/i18n'
+import { getAllPosts } from '@/src/services/BlogPostsService'
+import { makeAbsoluteUrl } from '@/src/services/UrlService'
+
+type Sitemap = MetadataRoute.Sitemap
+type Item = MetadataRoute.Sitemap[0]
+
+const mapLocales = (callback: (locale: string) => Sitemap) =>
+  ['', ...Object.values(Locales)].flatMap((locale) => callback(locale))
+
+const getUrl = (locale: string, url: string) =>
+  locale ? makeAbsoluteUrl(`/${locale}${url}`) : makeAbsoluteUrl(url)
+
+export default function sitemap(): Sitemap {
+  return [
+    ...mapLocales((locale) => [
+      {
+        url: getUrl(locale, '/'),
+        lastModified: new Date(),
+        changeFrequency: 'monthly',
+        priority: 1,
+      },
+      {
+        url: getUrl(locale, '/posts'),
+        lastModified: new Date(),
+        changeFrequency: 'daily',
+        priority: 0.9,
+      },
+      {
+        url: getUrl(locale, '/terms-of-use'),
+        lastModified: new Date(),
+        changeFrequency: 'yearly',
+        priority: 0.5,
+      },
+      ...(locale
+        ? getAllPosts(locale).flatMap(
+            (post) =>
+              ({
+                url: getUrl(locale, `/posts/${post.slug}`),
+                lastModified: post.date,
+                changeFrequency: 'yearly',
+                priority: 0.8,
+              }) as Item,
+          )
+        : []),
+    ]),
+    ...getAllPosts(DEFAULT_LOCALE).flatMap(
+      (post) =>
+        ({
+          url: getUrl('', `/posts/${post.slug}`),
+          lastModified: post.date,
+          changeFrequency: 'yearly',
+          priority: 0.8,
+        }) as Item,
+    ),
+  ]
+}
--- src/config/i18n.ts
+++ src/config/i18n.ts
@@ -2,6 +2,8 @@ import ptBR from '@/src/locales/pt-BR.json'
 import en from '@/src/locales/en.json'
 import { enGB as enGBDate, ptBR as ptBRDate } from 'date-fns/locale'
 
+export const DEFAULT_LOCALE = 'en'
+
 export enum Locales {
   en = 'en',
   ptBR = 'pt-BR',
